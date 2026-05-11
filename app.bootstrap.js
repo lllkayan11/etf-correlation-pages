7c6cb5a981c23a965aa7ad75e470c165fb98ef8c
LKayan <lkayan@KAYAN.local>
2026-05-11 22:05:23 +0800

    fix(pages): cache-bust compiled bundle import

--- app.bootstrap.js
+++ app.bootstrap.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./app.compiled.v2.js";
+import App from "./app.compiled.v2.js?v=e595a78";
 
 const rootElement = document.getElementById("root");
 if (rootElement) {
